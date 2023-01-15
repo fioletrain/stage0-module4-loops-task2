cc8162907ab3571486e4d6a75ac4e02790b01851
Svitlana D <svetk2801@gmail.com>
2023-01-15 17:16:26 +0200

    Module 4.2. Task 2. While Loops

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int counter = 1;
+        int a = 1;
+        int n = 1;
+        System.out.println(1);
+        while (counter <= printToInclusive) {
+            while (a <= counter) {
+                n = n * a;
+                a++;
+            }
+            System.out.println(n);
+            counter++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,30 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 3;
+        if (printToInclusive == 0) {
+        } else {
+            if (printToInclusive >= 7) {
+                System.out.println(2);
+                System.out.println(3);
+                System.out.println(5);
+                System.out.println(7);
+            } else if (printToInclusive == 5 || printToInclusive == 6) {
+                System.out.println(2);
+                System.out.println(3);
+                System.out.println(5);
+            } else if (printToInclusive == 3 || printToInclusive == 4) {
+                System.out.println(2);
+                System.out.println(3);
+            } else {
+                System.out.println(2);
+            }
+            while (counter <= printToInclusive) {
+                if ((counter % 2 != 0) && (counter % 3 != 0) && (counter % 5 != 0) && (counter % 7 != 0)) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        }
     }
 }
